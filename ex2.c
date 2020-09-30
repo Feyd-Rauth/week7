08d159f6a11488e6604d7753a6967de0e3971b00
Feyd-Rauth <atreides.pasha@yandex.ru>
2020-09-30 21:52:44 +0300

    Second task

---
+++ ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int n;
+	scanf("%d", &n);
+	int* arr = NULL;
+	arr= (int*) malloc(n*sizeof(int));
+	for (int i = 0; i < n; i++){
+		arr[i] = i;
+		printf("%d\n", arr[i]);
+	}
+	free(arr);
+}
